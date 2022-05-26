d1b7e5d0e69da8e22df42044e775d2ce45272e0e
tosankrisna <tosankrisna@gmail.com>
2022-05-26 14:04:03 +0800

    update model

--- Capstone-cc/api/models/disease.js
+++ Capstone-cc/api/models/disease.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association heres
       Disease.belongsTo(User);
     }
   }
